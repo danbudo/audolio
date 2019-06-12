0f6da7b5c5dd03734a61807bcf9f9aca4fdcb89d
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-12 16:41:06 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(function(){
+    var current = location.pathname;
+    $('nav a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
