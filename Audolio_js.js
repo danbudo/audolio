4dd48d9499eb38da805b01502944c8c49f35b6df
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-12 16:04:18 +0100

    Update Audolio_js.js

--- Audolio_js.js
+++ Audolio_js.js
@@ -1,7 +1,18 @@
 // JavaScript Document
 
-$(document).ready(function() {
+//$(document).ready(function() {
 	// get current URL path and assign 'active' class
-	var pathname = window.location.pathname;
-	$('nav > a[href="'+pathname+'"]').parent().addClass('active');
+//	var pathname = window.location.pathname;
+//	$('nav > a[href="'+pathname+'"]').parent().addClass('active');
+//      })
+
+$(function(){
+    var current = location.pathname;
+    $('nav a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
 })
