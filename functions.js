1d2b091c89af7e19824f607da744d1de95250661
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-13 15:14:11 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,30 +1,10 @@
 // JavaScript Document
 
-//$(function(){
-    //var current = location.pathname;
-    //$('nav a').each(function(){
-        //var $this = $(this);
-        // if the current path is like this link, make it active
-        //if($this.attr('href').indexOf(current) !== -1){
-        //    $this.addClass('active');
-        //}
-    //})
-//})
 
-//Show Secondary Nav
-$(document).ready(function(){
-  $("#showMenu").click(function(){
-	//$("#showMenu").addClass('active');
-    $("#collapsibleMenu").show();
-  });
-});
-
-// Hide Secondary Nav
-$(document).mouseup(function(e){
-    var container = $("#collapsibleMenu");
-
-    // If the target of the click isn't the container
-    if(!container.is(e.target) && container.has(e.target).length === 0){
-        container.hide();
-    }
-});
+// expand and collapse Secondary Nav
+<script>
+function expandCollapse() {
+   var element = document.getElementById("secondaryNav");
+   element.classList.toggle("expandMenu");
+}
+</script>
