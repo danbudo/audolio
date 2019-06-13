e8e2c97ded02411a3ac505517b84b7cf60515285
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-13 16:02:08 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,7 +1,17 @@
 // JavaScript Document
 
+
 // expand and collapse Secondary Nav
+
 function expandCollapse() {
 	var element = document.getElementById("secondaryNav");
 	element.classList.toggle("expandMenu");
 }
+
+// Add active class to nav items
+
+$(document).ready(function() {
+	// get current URL path and assign 'active' class
+	var pathname = window.location.pathname;
+	$('nav > a[href="'+pathname+'"]').parent().addClass('active');
+})
