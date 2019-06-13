01cb25e617eecb57ec7be1680655bf8f97d60255
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-13 16:37:05 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -13,5 +13,5 @@ function expandCollapse() {
 $(document).ready(function() {
 	// get current URL path and assign 'active' class
 	var pathname = window.location.pathname;
-	$('nav > a[href="'+pathname+'"]').parent().addClass('active');
+	$('nav > a[href="'+pathname+'"]').addClass('active');
 })
