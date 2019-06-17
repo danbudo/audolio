ad0619fd8a2e95c614bba6b1d687f5dcf1629c91
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-17 12:00:53 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -18,14 +18,14 @@ $(document).ready(function() {
 
 // Remove "expanded" class on mobile, until clicked
 
-function collapseMenuMobile(x) {
+function myFunction(x) {
   if (x.matches) { // If media query matches
     document.body.style.backgroundColor = "yellow";
   } else {
-   document.body.style.backgroundColor = "pink";
+    document.body.style.backgroundColor = "pink";
   }
 }
 
-var x = window.matchMedia("(max-width: 736px)")
-collapseMenuMobile(x) // Call listener function at run time
-x.addListener(collapseMenuMobile) // Attach listener function on state changes
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
