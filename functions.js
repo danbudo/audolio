192c7fbc08a9f35c6a8af29df0dafb3d2ff0a62a
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-18 17:37:47 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,13 +1,5 @@
 // JavaScript Document
 
-// Add active class to nav items
-
-//$(document).ready(function() {
-	// get current URL path and assign 'active' class
-	//var pathname = window.location.pathname;
-	//$('nav > a[href="'+pathname+'"]').addClass('active');
-//})
-
 // Add and remove "active" class to secondary navigation items
 
 $(document).ready(function(){
@@ -15,6 +7,10 @@ $(document).ready(function(){
     $('.secondaryNavLink').removeClass('active');
     $(this).addClass('active');
 });
+	
+	        if($(window).width() < 768) {
+           $("#secondaryNav").removeClass("expandMenu");  
+        }    
 });
 
 // expand and collapse Secondary Nav
@@ -26,8 +22,8 @@ function expandCollapse() {
 
 // Remove "expanded" class on mobile, until clicked
 
-$(document).ready(function () {
-        if($(window).width() < 768) {
-           $("#secondaryNav").removeClass("expandMenu");  
-        }    
-    });
+//$(document).ready(function () {
+//        if($(window).width() < 768) {
+//           $("#secondaryNav").removeClass("expandMenu");  
+//        }    
+//    });
