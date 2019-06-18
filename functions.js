aa47f1c919ae885ab6f6b5f39400ebeacfcfeb3e
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-18 11:18:36 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,11 +2,20 @@
 
 // Add active class to nav items
 
-$(document).ready(function() {
+//$(document).ready(function() {
 	// get current URL path and assign 'active' class
-	var pathname = window.location.pathname;
-	$('nav > a[href="'+pathname+'"]').addClass('active');
-})
+	//var pathname = window.location.pathname;
+	//$('nav > a[href="'+pathname+'"]').addClass('active');
+//})
+
+// Add and remove "active" class to secondary navigation items
+
+$(document).ready(function(){
+  $('.secondaryNavLink').click(function(){
+    $('.secondaryNavLink').removeClass('active');
+    $(this).addClass('active');
+});
+});
 
 // expand and collapse Secondary Nav
 
