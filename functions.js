510a90b8b53e5736c479312608c7b6c4e336df72
Dan Hahn <49907949+danbudo@users.noreply.github.com>
2019-06-19 14:22:23 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -2,12 +2,23 @@
 
 // Add and remove "active" class to secondary navigation items
 
-$(document).ready(function () {
-	$('.secondaryNavLink').click(function () {
-		$('secondaryNavLink').removeClass("active");
-		$(this).addClass("active");
-	});
-});
+//$(document).ready(function () {
+	//$('.secondaryNavLink').click(function () {
+		//$('secondaryNavLink').removeClass("active");
+		//$(this).addClass("active");
+	//});
+//});
+
+$(function(){
+    var current = location.pathname;
+    $('#secondaryNav a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
 
 // expand and collapse Secondary Nav
 
